bdee3d0256dd8820eafeeaf5e47a9acc08c2c1ac
Philipe Allan Almeida <philipe.almeida@outlook.com>
2018-02-09 17:43:39 -0200

    Debug da aplicação

--- config.php
+++ config.php
@@ -5,6 +5,6 @@ header('Content-Type: text/html; charset=utf-8');
 require 'vendor/autoload.php';
 
 // Load a single file
-$conf = \Noodlehaus\Config::load('../config.ini');
+$conf = \Noodlehaus\Config::load('/home/philipe.almeida/Code/app/config.ini');
 
 var_dump($conf);
--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
 
 require '../config.php';
+
+
 var_dump($conf);
